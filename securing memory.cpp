efeeafe1472f447af17d2131ea7dc928268c2d32
mohammadjamal18 <mohammadjamalabdaullah18@gmail.com>
2026-02-06 02:17:45 +0300

    Add files via upload

---
+++ securing memory.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+struct MemoryLab {
+    int data[2] = { 10, 20 }; // مصفوفة صغيرة جداً
+    int secretKey = 9999;    // البيانات الحساسة خلفها مباشرة
+};
+
+int main() {
+    MemoryLab lab;
+    int index;
+
+    cout << "Enter index to read (0-1): ";
+    cin >> index;
+
+    // جرب إدخال رقم 2
+    // بما أن المصفوفة حجمها 2 (0 و 1)، فالعنصر رقم 2 هو فعلياً بداية مكان secretKey
+    cout << "Value at index " << index << " is: " << lab.data[index] << endl;
+
+    return 0;
+}
